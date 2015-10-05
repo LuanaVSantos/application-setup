9a0e859e58b02b1c5ebb45bac25165b27ba1ccfe
LuanaVSantos <lsantos1@hawk.iit.edu>
2015-10-04 23:43:36 -0500

    Added code to pull index.html

--- install-webserver.sh
+++ install-webserver.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y apache2
+sudo apt-get install -y apache2 git
+
+git clone https://github.com/LuanaVSantos/env-setup.git
+
+cp ./env-setup/images /var/www/html/images
+cp ./env-setup/index.html /var/www/html
 
 echo "Hello!" > /tmp/hello.txt
